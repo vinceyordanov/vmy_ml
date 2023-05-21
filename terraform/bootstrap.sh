e2767bec4ecca3afb38f40dce5c189be74288f8b
vinceyordanov <105275666+vinceyordanov@users.noreply.github.com>
2023-05-21 14:48:09 +0200

    Updated the lifecycle file

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,10 +1,10 @@
 # -- Run this whenever initializing the terraform configuration for the first time -- #
 
-gcloud storage buckets create gs://r-server-326920-tf-states \
-    --project=r-server-326920 \
-    --default-storage-class=standard \
-    --location=europe-west4
+# gcloud storage buckets create gs://r-server-326920-tf-states \
+#     --project=r-server-326920 \
+#     --default-storage-class=standard \
+#     --location=europe-west4
 
   gcloud storage buckets update gs://r-server-326920-tf-states \
     --versioning \
-    --lifecycle-file="./terraform/tf-state-bucket-lifecycle.json"
+    --lifecycle-file="./tf-state-bucket-lifecycle.json"
